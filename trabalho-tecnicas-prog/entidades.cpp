ecdfae118713de8808f36f7636f53dc58067f91d
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-10-18 18:02:25 -0300

    Update

--- trabalho-tecnicas-prog/entidades.cpp
+++ trabalho-tecnicas-prog/entidades.cpp
@@ -1,40 +1,4 @@
-#include "entidades.h"
-#include "dominios.h"
-#include <iostream>
-#include <vector>
-#include <stdexcept>
-#include <regex>
-#include <set>
 
-// Implementação dos métodos da classe Usuario
-
-void Usuario::criarConta(string email, string senha) {
-    Conta novaConta;
-
-    try {
-        // Validação do email
-        Email emailDominio;
-        emailDominio.setValor(email);
-
-        // Validação da senha
-        Senha senhaDominio;
-        senhaDominio.setValor(senha);
-
-        novaConta.setEmail(email);
-        novaConta.setSenha(senha);
-
-        conta = novaConta;
-
-  
-    } catch (const std::invalid_argument &e) {
-        
-        std::cerr << "Erro ao criar conta: " << e.what() << std::endl;
-    }
-}
-
-Conta Usuario::getConta() {
-    return conta;
-}
 
 
 
