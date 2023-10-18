cc08539ffe15e3f4e78aa2a6b2c2ea41ac651150
VitorDanelon <vitorcdanelon@gmail.com>
2023-10-18 18:12:44 -0300

    Arrumando um pouco do trabalho
    Arrumamos algumas coisas.

--- trabalho-tecnicas-prog/dominios.cpp
+++ trabalho-tecnicas-prog/dominios.cpp
@@ -1,39 +1,93 @@
 #include "dominios.h"
-
+#include <stdexcept>
+using namespace std;
 // --------------------------------------------------------------------------
 // Implementações de métodos de classe domínio.
 
 // --------------------------------------------------------------------------
 // Método para validação de valor.
 
-void Codigo::validar(int codigo){
-    if (codigo > LIMITE)
-        throw invalid_argument("Argumento invalido.");
+//void Codigo::validar(int codigo){
+//    if (codigo > LIMITE)
+//        throw invalid_argument("Argumento invalido.");
+//}
+
+void Conta::setEmail(const std::string& email) {
+    if (!validarEmail(email)) {
+        throw std::invalid_argument("Email inválido.");
+    }
+    this->email = email;
 }
 
-// --------------------------------------------------------------------------
-// Método para armazenamento de valor.
-
-void Codigo::setValor(int valor){
-    validar(valor);
-    this->valor = valor;
+void Conta::setSenha(const std::string& senha) {
+    if (!validarSenha(senha)) {
+        throw std::invalid_argument("Senha inválida.");
+    }
+    this->senha = senha;
 }
 
-// --------------------------------------------------------------------------
-// Implementações de métodos de classe domínio.
-
-// --------------------------------------------------------------------------
-// Método para validação de valor.
-
-void Prioridade::validar(int valor){
-    if (valor == INVALIDO)
-        throw invalid_argument("Argumento invalido.");
+string Conta::getEmail() const {
+    return email;
 }
 
-// --------------------------------------------------------------------------
-// Método para armazenamento de valor.
-
-void Prioridade::setValor(int valor) {
-    validar(valor);
-    this->valor = valor;
+string Conta::getSenha() const {
+    return senha;
 }
+
+bool Conta::validarEmail(const std::string& email) {
+    const std::regex emailRegex(R"([a-zA-Z0-9._%+-]{2,10}@[a-zA-Z0-9.-]{2,20})");
+    if (!std::regex_match(email, emailRegex)) {
+        return false;
+    }
+
+    size_t atPos = email.find('@');
+    if (atPos != std::string::npos) {
+        if (atPos > 0 && email[atPos - 1] == '.') {
+            return false;
+        }
+        if (atPos < email.length() - 1 && email[atPos + 1] == '.') {
+            return false;
+        }
+    }
+
+    size_t dotPos = email.find("..");
+    if (dotPos != std::string::npos) {
+        return false;
+    }
+
+    return true;
+}
+
+bool Conta::validarSenha(const std::string& senha) {
+    // Pelo menos um caractere é letra maiúscula
+    bool temMaiuscula = false;
+    // Pelo menos um caractere é letra minúscula
+    bool temMinuscula = false;
+    // Pelo menos um caractere é dígito
+    bool temDigito = false;
+    // Pelo menos um caracter é sinal de pontuação
+    bool temPonto = false;
+
+    std::set<char> caracteresDuplicados;
+
+    for (char c : senha) {
+        if (isupper(c)) {
+            temMaiuscula = true;
+        } else if (islower(c)) {
+            temMinuscula = true;
+        } else if (isdigit(c)) {
+            temDigito = true;
+        } else if (ispunct(c)) {
+            temPonto = true;
+        }
+
+        if (caracteresDuplicados.count(c) > 0) {
+            return false;  // Caractere duplicado encontrado
+        }
+
+        caracteresDuplicados.insert(c);
+    }
+
+    return temMaiuscula && temMinuscula && temDigito && temPonto;
+}
+};
--- trabalho-tecnicas-prog/dominios.h
+++ trabalho-tecnicas-prog/dominios.h
@@ -5,156 +5,6 @@
 
 using namespace std;
 
-/* -----------------------------------------------------------------------
-// Estrutura de código para declaração de classe domínio.
-//
-// Essa estrutura de código pode ser aproveitada na declaração de domínio.
-//
-// Seguem as atividades para adaptação:
-//
-// Substituir Dominio por nome da classe.
-//
-// Substituir Tipo pelo nome do tipo adequado.
-
-class Dominio {
-    private:
-        Tipo valor;                      // Atributo para armazenar valor.
-        void validar(Tipo);              // Método para validar valor.
-    public:
-        void setValor(Tipo);             // Método para atribuir valor.
-        Tipo getValor() const;           // Método para recuperar valor.
-};
-
-inline Tipo Dominio::getValor() const{
-    return valor;
-}
-
------------------------------------------------------------------------ */
-
-// -----------------------------------------------------------------------
-// Exemplos de uso da estrutura de código anterior.
-
-// -----------------------------------------------------------------------
-// Exemplo de declaração de classe domínio e implementação de método.
-
-/*class Codigo {
-private:
-        static const int LIMITE = 25;       // Declaração para evitar números mágicos.
-        int valor;
-        void validar(int);
-public:
-        void setValor(int);
-        int getValor() const;
-};
-
-inline int Codigo::getValor() const{
-    return valor;
-}*/
-
-// -----------------------------------------------------------------------
-// Exemplo de declaração de classe domínio e implementação de método.
-
-/*class Prioridade {
-    private:
-        static const int INVALIDO = 0;      // Declaração para evitar números mágicos.
-        int valor;
-        void validar(int);
-    public:
-        void setValor(int);
-        int getValor() const;
-};
-
-inline int Prioridade::getValor() const{
-    return valor;
-}*/
-
-class Conta {
-private:
-    std::string email;
-    std::string senha;
-
-public:
-    void setEmail(const std::string& email) {
-        if (!validarEmail(email)) {
-            throw std::invalid_argument("Email inválido.");
-        }
-        this->email = email;
-    }
-
-    void setSenha(const std::string& senha) {
-        if (!validarSenha(senha)) {
-            throw std::invalid_argument("Senha inválida.");
-        }
-        this->senha = senha;
-    }
-
-    std::string getEmail() const {
-        return email;
-    }
-
-    std::string getSenha() const {
-        return senha;
-    }
-
-private:
-    bool validarEmail(const std::string& email) {
-        const std::regex emailRegex(R"([a-zA-Z0-9._%+-]{2,10}@[a-zA-Z0-9.-]{2,20})");
-        if (!std::regex_match(email, emailRegex)) {
-            return false;
-        }
-
-        size_t atPos = email.find('@');
-        if (atPos != std::string::npos) {
-            if (atPos > 0 && email[atPos - 1] == '.') {
-                return false;
-            }
-            if (atPos < email.length() - 1 && email[atPos + 1] == '.') {
-                return false;
-            }
-        }
-
-        size_t dotPos = email.find("..");
-        if (dotPos != std::string::npos) {
-            return false;
-        }
-
-        return true;
-    }
-
-    bool validarSenha(const std::string& senha) {
-        // Pelo menos um caractere é letra maiúscula
-        bool temMaiuscula = false;
-        // Pelo menos um caractere é letra minúscula
-        bool temMinuscula = false;
-        // Pelo menos um caractere é dígito
-        bool temDigito = false;
-        // Pelo menos um caracter é sinal de pontuação
-        bool temPonto = false;
-
-        std::set<char> caracteresDuplicados;
-
-        for (char c : senha) {
-            if (isupper(c)) {
-                temMaiuscula = true;
-            } else if (islower(c)) {
-                temMinuscula = true;
-            } else if (isdigit(c)) {
-                temDigito = true;
-            } else if (ispunct(c)) {
-                temPonto = true;
-            }
-
-            if (caracteresDuplicados.count(c) > 0) {
-                return false;  // Caractere duplicado encontrado
-            }
-
-            caracteresDuplicados.insert(c);
-        }
-
-        return temMaiuscula && temMinuscula && temDigito && temPonto;
-    }
-};
-
 class Conta{
     private:
         string nome;
@@ -169,6 +19,9 @@ class Conta{
         string getEmail() const;
         void setSenha(string);
         string getSenha() const;
+        bool validarEmail(string);
+        bool validarSenha(string);
+
         //void editarCredenciais();
         //string visualizarCredenciais();
 
