ffa4e3f209678dcd722313c54aa6faf0180d3ee7
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-10-18 17:43:25 -0300

    Update

--- trabalho-tecnicas-prog/entidades.cpp
+++ trabalho-tecnicas-prog/entidades.cpp
@@ -36,44 +36,5 @@ Conta Usuario::getConta() {
     return conta;
 }
 
-void Usuario::criarQuadro() {
-    Quadro novoQuadro;
-
-    try {
-      
-        std::string nomeQuadro;
-        std::cout << "Digite o nome do novo quadro: ";
-        std::cin >> nomeQuadro;
-
-        Nome nomeDominio;
-        nomeDominio.setValor(nomeQuadro);
-
-        
-        std::cout << "Digite a descrição do novo quadro: ";
-        std::string descricaoQuadro;
-        std::cin.ignore(); // Limpa o buffer do teclado
-        getline(std::cin, descricaoQuadro);
-        Texto descricaoDominio;
-        descricaoDominio.setValor(descricaoQuadro);
-
-        
-        int limiteQuadro;
-        std::cout << "Digite o limite do quadro: ";
-        std::cin >> limiteQuadro;
-        Limite limiteDominio;
-        limiteDominio.setValor(limiteQuadro);
-
-        novoQuadro.setNome(nomeQuadro);
-        novoQuadro.setDescricao(descricaoQuadro);
-        novoQuadro.setLimite(limiteQuadro);
-
-        quadro = novoQuadro;
-
-        
-    } catch (const std::invalid_argument &e) {
-        
-        std::cerr << "Erro ao criar quadro: " << e.what() << std::endl;
-    }
-}
 
 
