2609d9e2e944303420c08ed110784031853cf8b1
VitorDanelon <vitorcdanelon@gmail.com>
2023-10-19 21:40:48 -0300

    Instruções para o projeto
    ...

--- trabalho-tecnicas-prog/dominios.h
+++ trabalho-tecnicas-prog/dominios.h
@@ -5,72 +5,34 @@
 
 using namespace std;
 
-class Conta{
-    private:
-        string nome;
-        string email;
-        string senha;
-        void validar(string);
-
-    public:
-        void setNome(string);
-        string getNome();
-        void setEmail(string);
-        string getEmail() const;
-        void setSenha(string);
-        string getSenha() const;
-        bool validarEmail(string);
-        bool validarSenha(string);
-
-        //void editarCredenciais();
-        //string visualizarCredenciais();
-
-};
-
-//Vitor
-class Quadro{
-    private:
-    char codigo;
-    string nome;
-    string descricao;
-    int limite;
-
-    public:
+//Código (Luca Vitor)
         void setCodigo(char);
         char getCodigo();
-        void setNome(string);
-        string getNome();
-        void setDescricao(string);
-        string getDescricao();
-        void setLimite(int);
-        int getLimite();
-        bool validarTexto(const std::string& texto);
         static bool validarCodigo(const std::string& codigo);
 
-};
-
-//Luca
-
-class Cartao{
-    private:
-    char codigo;
-    string nome;
-    string descricao;
-    int limite;
-
-    public:
-        void setCodigo(char);
-        char getCodigo();
-        void setNome(string);
-        string getNome();
-        void setDescricao(string);
-        string getDescricao();
+//Coluna (Luca)
         void setColuna(string);
         string getColuna();
-};
+        //VALIDAR COLUNA
 
+//Email (Raphael)
+    bool validarEmail(string);
+    void setEmail(string);
+    string getEmail() const;
 
+//Limite (Vitor)
+    void setLimite(int);
+    int getLimite();
+    //VALIDAR LIMITE
 
+//Senha (Raphael)
+    bool validarSenha(string);
+    void setSenha(string);
+    string getSenha() const;
 
+//Texto (Raphael Luca Vitor)
+        bool validarTexto(const std::string& texto);
+        //GET TEXTO
+        //SET TEXTO
 
 #endif // DOMINIOS_H_INCLUDED
--- trabalho-tecnicas-prog/entidades.h
+++ trabalho-tecnicas-prog/entidades.h
@@ -4,122 +4,53 @@
 #include "dominios.h"
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
-/* -----------------------------------------------------------------------
-// Estrutura de código para declaração de classe entidade.
-//
-// Essa estrutura de código pode ser aproveitada na declaração de entidade.
-//
-// Seguem as atividades para adaptação:
-//
-// Substituir Entidade por nome da classe.
-//
-// Substituir Dominio.
-//
-// Substituir nomeAtributo.
-
-class Entidade {
+//Raphael
+class Conta{
     private:
-            Dominio nomeAtributo;                   // Declarar cada atributo da classe.
+        Email email;
+        Texto nome;
+        Senha senha;
+
     public:
-            void setnomeAtributo(const Dominio&);   // Declarar método set para cada atributo.
-            Dominio getnomeAtributo() const;        // Declarar método get para cada atributo.
+        //Criar conta
+        //Visualizar conta
+        //Editar conta
+        //Eliminar conta
+
 };
 
-inline void Entidade::setnomeAtributo(const Dominio &nomeAtributo){
-    this->nomeAtributo = nomeAtributo;
-}
+//Vitor
+class Quadro{
+    private:
+    Codigo codigo;
+    Texto nome;
+    Texto descricao;
+    Limite limite;
 
-inline Dominio Entidade::getnomeAtributo() const{
-    return nomeAtributo;
-}
+    public:
+    //Criar Quadro
+    //Visualizar Quadro
+    //Eliminar Quadro
 
------------------------------------------------------------------------ */
-
-// -----------------------------------------------------------------------
-// Exemplos de uso da estrutura de código anterior.
-
-// -----------------------------------------------------------------------
-// Exemplo de declaração de classe entidade e implementações de métodos.
-
-/*class Projeto {
-private:
-    Codigo      codigo;
-    Prioridade  prioridade;
-public:
-    void setCodigo(const Codigo&);
-    Codigo getCodigo() const;
-    void setPrioridade(const Prioridade&);
-    Prioridade getPrioridade() const;
 };
 
-inline void Projeto::setCodigo(const Codigo& codigo){
-    this->codigo = codigo;
-}
+//Luca
+class Cartao{
+    private:
+    Codigo codigo;
+    Texto nome;
+    Texto descricao;
+    Coluna coluna;
 
-inline Codigo Projeto::getCodigo() const {
-    return codigo;
-}
-
-inline void Projeto::setPrioridade(const Prioridade& prioridade){
-    this->prioridade = prioridade;
-}
-
-inline Prioridade Projeto::getPrioridade() const {
-    return prioridade;
-}*/
-
-
-
-//class Usuario {
-//private:
-    //Conta conta;
-    //Quadro quadro;
-    //vector<Cartao> cartoes;
-
-//public:
-    //void criarUsuario();
-   // Conta getConta();
-    //void criarQuadro();
-    //Quadro getQuadro();
-    //void criarCartao();
-    //vector<Cartao> getCartoes();
-
-
-//};
-
-
-class Usuario {
-private:
-    Conta conta;
-    Quadro quadro;
-    std::vector<Cartao> cartoes;
-
-public:
-    Usuario(std::string nome, std::string email, std::string senha);
-
-    std::string getNome();
-    std::string getEmail();
-
-    vector visualizarConta();
-    void editarConta();
-    void eliminarConta();
-
-    bool validarSenha(std::string senha);
-
-    void criarQuadro(char codigo, std::string nome, std::string descricao, int limite);
-    vector visualizarQuadro(char codigo);
-    void eliminarQuadro();
-
-    void criarCartao(char codigo, std::string nome, std::string descricao, int limite, std::string coluna);
-    vector visualizarCartao(char codigo)
-    void eliminarCartao(char codigo);
-    void editarCartao(char codigo);
-    void moverCartao(char codigo);
-    void listarCartoes();
+    public:
+        //Criar cartão
+        //Visualizar cartão
+        //Mover cartão
+        //Eliminar cartão
 };
 
-
-#endif // ENTIDADES_H_INCLUDED
+#endif
