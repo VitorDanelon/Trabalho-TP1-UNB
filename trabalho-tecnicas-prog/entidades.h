81e835ca06c734b8238d1e3db695f480992830a1
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-10-22 09:11:06 -0300

    Update entidades.h

--- trabalho-tecnicas-prog/entidades.h
+++ trabalho-tecnicas-prog/entidades.h
@@ -3,54 +3,81 @@
 
 #include "dominios.h"
 #include <string>
-#include <vector>
-#include <stdexcept>
 
 using namespace std;
 
-//Raphael
-class Conta{
-    private:
-        Email email;
-        Texto nome;
-        Senha senha;
+//matricula: 222033157
+/**
+ * @brief A classe Conta representa uma conta de usuário.
+ *
+ * Esta classe contém informações sobre a conta, como email, nome e senha, e fornece
+ * métodos para visualização, edição e exclusão da conta.
+ */
+class Conta {
+private:
+    Email email; /**< Email associado à conta. */
+    Texto nome; /**< Nome associado à conta. */
+    Senha senha; /**< Senha associada à conta. */
 
-    public:
-        //Criar conta
-        //Visualizar conta
-        //Editar conta
-        //Eliminar conta
-
+public:
+    /**
+     * @brief Método que constrói uma unidade da classe.
+     *
+     *
+     */
+    Conta(const string& email, const string& nome, const string& senha);
+    /**
+     * @brief Visualiza os dados da conta.
+     *
+     * @return Uma string que contém os dados da conta formatados para exibição.
+     */
+    string visualizarConta() const;
+    /**
+     * @brief Edita o nome e/ou a senha da conta.
+     *
+     * @param novoNome O novo nome a ser definido para a conta.
+     * @param novaSenha A nova senha a ser definida para a conta.
+     */
+    void editarConta(const string& novoNome, const string& novaSenha);
+    /**
+     * @brief Elimina a conta, redefinindo seus atributos para valores vazios.
+     */
+    void eliminarConta();
 };
 
-//Vitor
-class Quadro{
-    private:
+//Vitor classe quadro
+class Quadro {
+private:
     Codigo codigo;
     Texto nome;
     Texto descricao;
     Limite limite;
 
-    public:
-    //Criar Quadro
-    //Visualizar Quadro
-    //Eliminar Quadro
+public:
+    Quadro(const string& codigo, const string& nome, const string& descricao, const string& limite);
 
+    string visualizarQuadro() const;
+
+    void eliminarQuadro();
 };
 
-//Luca
-class Cartao{
-    private:
+// Classe Cartao
+class Cartao {
+private:
     Codigo codigo;
     Texto nome;
     Texto descricao;
     Coluna coluna;
     bool excluido;
 
-    public:
-    void visualizarCartao();
+public:
+    Cartao(const Codigo& codigo, const Texto& nome, const Texto& descricao, const Coluna& coluna);
+
+    string visualizarCartao() const;
+
     void moverCartao(Coluna novaColuna);
+
     void eliminarCartao();
 };
-//Alo
+
 #endif
