9d31a6d68678a33cb8829f2a7fb67e09ee4097c6
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-10-22 09:28:39 -0300

    Update testes.h

--- trabalho-tecnicas-prog/testes.h
+++ trabalho-tecnicas-prog/testes.h
@@ -1,9 +1,9 @@
-// Diretivas de compilação para controlar processo de inclusão.
+// Diretivas de compilaÃ§Ã£o para controlar processo de inclusÃ£o.
 
 #ifndef TESTES_H_INCLUDED
 #define TESTES_H_INCLUDED
 
-// Incluir cabeçalhos.
+// Incluir cabeÃ§alhos.
 
 #include "dominios.h"
 #include "entidades.h"
@@ -11,40 +11,50 @@
 using namespace std;
 
 // --------------------------------------------------------------------------
-// Exemplo de declaração de classe para teste de unidade de classe domínio.
+// Exemplo de declaraÃ§Ã£o de classe para teste de unidade de classe domÃ­nio.
 
 /*class TUCodigo {
 private:
-    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
-    const static int VALOR_INVALIDO = 30;   // Definição de constante para evitar número mágico.
-    Codigo *codigo;                         // Referência para unidade em teste.
+    const static int VALOR_VALIDO   = 20;   // DefiniÃ§Ã£o de constante para evitar nÃºmero mÃ¡gico.
+    const static int VALOR_INVALIDO = 30;   // DefiniÃ§Ã£o de constante para evitar nÃºmero mÃ¡gico.
+    Codigo *codigo;                         // ReferÃªncia para unidade em teste.
     int estado;                             // Estado do teste.
-    void setUp();                           // Método para criar unidade em teste.
-    void tearDown();                        // Método para destruir unidade em teste.
-    void testarCenarioSucesso();            // Cenário de teste.
-    void testarCenarioFalha();              // Cenário de teste.
+    void setUp();                           // MÃ©todo para criar unidade em teste.
+    void tearDown();                        // MÃ©todo para destruir unidade em teste.
+    void testarCenarioSucesso();            // CenÃ¡rio de teste.
+    void testarCenarioFalha();              // CenÃ¡rio de teste.
 
 public:
-    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
-    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
-    int run();                              // Método para executar teste.
+    const static int SUCESSO =  0;          // DefiniÃ§Ã£o de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // DefiniÃ§Ã£o de constante para reportar resultado de teste.
+    int run();                              // MÃ©todo para executar teste.
 };
 
 // --------------------------------------------------------------------------
-// Exemplo de declaração de classe para teste de unidade de classe entidade.
+// Exemplo de declaraÃ§Ã£o de classe para teste de unidade de classe entidade.
 
 /*class TUProjeto {
 private:
-    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
-    Projeto *projeto;                       // Referência para unidade em teste.
+    const static int VALOR_VALIDO   = 20;   // DefiniÃ§Ã£o de constante para evitar nÃºmero mÃ¡gico.
+    Projeto *projeto;                       // ReferÃªncia para unidade em teste.
     int estado;                             // Estado do teste.
-    void setUp();                           // Método para criar unidade em teste.
-    void tearDown();                        // Método para destruir unidade em teste.
-    void testarCenarioSucesso();            // Cenário de teste.
+    void setUp();                           // MÃ©todo para criar unidade em teste.
+    void tearDown();                        // MÃ©todo para destruir unidade em teste.
+    void testarCenarioSucesso();            // CenÃ¡rio de teste.
 public:
-    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
-    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
-    int run();                              // Método para executar teste.
+    const static int SUCESSO =  0;          // DefiniÃ§Ã£o de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // DefiniÃ§Ã£o de constante para reportar resultado de teste.
+    int run();                              // MÃ©todo para executar teste.
 };*/
+class TUColuna {
+public:
+    static const int SUCESSO = 0;
+    static const int FALHA = 1;
 
+    int run();
+
+private:
+    void testarCenarioValido();
+    void testarCenarioInvalido();
+};
 #endif // TESTES_H_INCLUDED
