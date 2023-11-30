bd605177e6e9b0e92ba27a4b9802597e951a9466
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-11-30 11:40:09 -0300

    Update interfaces.h

--- interfaces.h
+++ interfaces.h
@@ -22,10 +22,6 @@ public:
     virtual void criarQuadro(const string& codigo, const string& nome, const string& descricao, const string& limite) = 0;
     virtual void visualizarQuadro(const string& codigo) = 0;
     virtual void eliminarQuadro(const string& codigo) = 0;
-    virtual void criarCartao(const string& codigoQuadro, const string& codigoCartao, const string& nome, const string& descricao, const string& coluna) = 0;
-    virtual void visualizarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual void moverCartao(const string& codigoQuadro, const string& codigoCartao, const string& novaColuna) = 0;
-    virtual void eliminarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
     virtual void setCntrLNQuadro(ILNQuadro *cntrLNQuadro) = 0;
     virtual ~IUQuadro() {}  // Método destrutor virtual.
 };
@@ -38,10 +34,6 @@ public:
     virtual void criarQuadro(const string& codigo, const string& nome, const string& descricao, const string& limite) = 0;
     virtual Quadro visualizarQuadro(const string& codigo) = 0;
     virtual void eliminarQuadro(const string& codigo) = 0;
-    virtual void criarCartao(const string& codigoQuadro, const string& codigoCartao, const string& nome, const string& descricao, const string& coluna) = 0;
-    virtual void visualizarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual void moverCartao(const string& codigoQuadro, const string& codigoCartao, const string& novaColuna) = 0;
-    virtual void eliminarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
     virtual ~ILNQuadro() {}  // Método destrutor virtual.
 };
 
