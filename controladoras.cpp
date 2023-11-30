6707a916cca1593a063226a27ebc4578d9177c78
Luca Verdade Lenzoni <88348508+lucaverdade@users.noreply.github.com>
2023-11-30 11:09:42 -0300

    Update controladoras.cpp

--- controladoras.cpp
+++ controladoras.cpp
@@ -1,12 +1,64 @@
+#include "interfaces.h"
 #include "controladoras.h"
+#include "dominios.h"
+#include "dominios.h"
+#include <iostream>
+#include <string>
 
+Quadro::Quadro(const string& codigo, const string& nome, const string& descricao, const string& limite) {
+    this->codigo.setCodigo(codigo);
+    this->nome.setTexto(nome);
+    this->descricao.setTexto(descricao);
+    this->limite.setLimite(limite);
+}
+
+
+
+string Quadro::visualizarQuadro() const {
+    return "Quadro: " + codigo.getCodigo() + "\nNome: " + nome.getTexto() + "\nDescri√ß√£o: " + descricao.getTexto() + "\nLimite: " + limite.getLimite();
+
+}
+
+void Quadro::eliminarQuadro(){
+    codigo.setCodigo("");
+    nome.setTexto("");
+    descricao.setTexto("");
+    limite.setLimite("");
+}
+
+Cartao::Cartao(const string& codigo, const string& nome, const string& descricao, const string& coluna){
+    this->codigo.setCodigo(codigo);
+    this->nome.setTexto(nome);
+    this->descricao.setTexto(descricao);
+    this->coluna.setColuna(coluna);
+
+}
+
+string Cartao::visualizarCartao() {
+
+    return "Codigo: " + codigo.getCodigo() + "\nNome: " + nome.getTexto() + "\nDescricao: " + descricao.getTexto() + "\nColuna: " + coluna.getColuna();
+
+
+}
+
+void Cartao::moverCartao(const string& coluna) {
+    this->coluna.setColuna(coluna);
+}
+
+void Cartao::eliminarCartao() {
+    codigo.setCodigo("");
+    nome.setTexto("");
+    descricao.setTexto("");
+    coluna.setColuna("SOLICITADO");
+
+}
 // ---------------------------------------------------------------------------------
-// ImplementaÁıes de mÈtodos de classe controladora.
+// Implementa√ß√µes de m√©todos de classe controladora.
 
 void CntrIUQuadro::criarQuadro(const string& codigo, const string& nome, const string& descricao, const string& limite) {
     try {
 
-        // Chamada do mÈtodo da camada de lÛgica de negÛcios para criar um quadro.
+        // Chamada do m√©todo da camada de l√≥gica de neg√≥cios para criar um quadro.
         cntrLNQuadro->criarQuadro(codigo, nome, descricao, limite);
 
         cout << "Quadro criado com sucesso!" << endl;
@@ -18,19 +70,22 @@ void CntrIUQuadro::criarQuadro(const string& codigo, const string& nome, const s
 
 void CntrIUQuadro::visualizarQuadro(const string& codigo) {
     try {
-        // 1. Verificar se o cÛdigo do quadro È v·lido.
-        if (!Codigo::validarCodigo(codigo)) {
-            throw invalid_argument("CÛdigo de quadro inv·lido.");
-        }
+        // 1. Verificar se o c√≥digo do quadro √© v√°lido.
+        Codigo codigoObjeto;
+            if (!codigoObjeto.validarCodigo(codigo)) {
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para visualizar o quadro.
-        Quadro quadro = cntrLNQuadro->visualizarQuadro(codigo);
+            cout << "Erro ao ver o quadro: ";
+            }
 
-        // 3. Exibir as informaÁıes do quadro.
+
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para visualizar o quadro.
+        Quadro quadro = cntrLNQuadro-> visualizarQuadro(codigo);
+
+        // 3. Exibir as informa√ß√µes do quadro.
         cout << "Detalhes do Quadro:" << endl;
-        cout << "CÛdigo: " << quadro.getCodigo().getCodigo() << endl;
+        cout << "C√≥digo: " << quadro.getCodigo().getCodigo() << endl;
         cout << "Nome: " << quadro.getNome().getTexto() << endl;
-        cout << "DescriÁ„o: " << quadro.getDescricao().getTexto() << endl;
+        cout << "Descri√ß√£o: " << quadro.getDescricao().getTexto() << endl;
         cout << "Limite: " << quadro.getLimite().getLimite() << endl;
 
     } catch (const exception& exp) {
@@ -41,12 +96,12 @@ void CntrIUQuadro::visualizarQuadro(const string& codigo) {
 
 void CntrIUQuadro::eliminarQuadro(const string& codigo) {
     try {
-        // 1. Verificar se o cÛdigo do quadro È v·lido.
+        // 1. Verificar se o c√≥digo do quadro √© v√°lido.
         if (!Codigo::validarCodigo(codigo)) {
-            throw invalid_argument("CÛdigo de quadro inv·lido.");
+            throw invalid_argument("C√≥digo de quadro inv√°lido.");
         }
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para eliminar o quadro.
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para eliminar o quadro.
         cntrLNQuadro->eliminarQuadro(codigo);
 
         cout << "Quadro eliminado com sucesso!" << endl;
@@ -59,69 +114,92 @@ void CntrIUQuadro::eliminarQuadro(const string& codigo) {
 
 void CntrIUQuadro::criarCartao(const string& codigoQuadro, const string& codigoCartao, const string& nome, const string& descricao, const string& coluna) {
     try {
-        // 1. Verificar se os cÛdigos e par‚metros s„o v·lidos.
+        // 1. Verificar se os c√≥digos e par√¢metros s√£o v√°lidos.
         if (!Codigo::validarCodigo(codigoQuadro) || !Codigo::validarCodigo(codigoCartao) ||
             !Texto::validarTexto(nome) || !Texto::validarTexto(descricao) || !Coluna::validarColuna(coluna)) {
-            throw invalid_argument("Par‚metros inv·lidos para criar o cart„o.");
+            throw invalid_argument("Par√¢metros inv√°lidos para criar o cart√£o.");
         }
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para criar o cart„o.
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para criar o cart√£o.
         cntrLNQuadro->criarCartao(codigoQuadro, codigoCartao, nome, descricao, coluna);
-        cout << "Cart„o criado com sucesso!" << endl;
+        cout << "Cart√£o criado com sucesso!" << endl;
 
     } catch (const exception& exp) {
-        cout << "Erro ao criar cart„o: " << exp.what() << endl;
+        cout << "Erro ao criar cart√£o: " << exp.what() << endl;
     }
 }
 
 
 void CntrIUQuadro::visualizarCartao(const string& codigoQuadro, const string& codigoCartao) {
     try {
-        // 1. Verificar se os cÛdigos s„o v·lidos.
+        // 1. Verificar se os c√≥digos s√£o v√°lidos.
         if (!Codigo::validarCodigo(codigoQuadro) || !Codigo::validarCodigo(codigoCartao)) {
-            throw invalid_argument("CÛdigos inv·lidos para visualizar o cart„o.");
+            throw invalid_argument("C√≥digos inv√°lidos para visualizar o cart√£o.");
         }
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para visualizar o cart„o.
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para visualizar o cart√£o.
         cntrLNQuadro->visualizarCartao(codigoQuadro, codigoCartao);
-        // Exiba as informaÁıes do cart„o conforme necess·rio.
+        // Exiba as informa√ß√µes do cart√£o conforme necess√°rio.
 
     } catch (const exception& exp) {
-        cout << "Erro ao visualizar cart„o: " << exp.what() << endl;
+        cout << "Erro ao visualizar cart√£o: " << exp.what() << endl;
     }
 }
 
 
 void CntrIUQuadro::moverCartao(const string& codigoQuadro, const string& codigoCartao, const string& novaColuna) {
     try {
-        // 1. Verificar se os cÛdigos e a nova coluna s„o v·lidos.
+        // 1. Verificar se os c√≥digos e a nova coluna s√£o v√°lidos.
         if (!Codigo::validarCodigo(codigoQuadro) || !Codigo::validarCodigo(codigoCartao) || !Coluna::validarColuna(novaColuna)) {
-            throw invalid_argument("CÛdigos ou coluna inv·lidos para mover o cart„o.");
+            throw invalid_argument("C√≥digos ou coluna inv√°lidos para mover o cart√£o.");
         }
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para mover o cart„o.
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para mover o cart√£o.
         cntrLNQuadro->moverCartao(codigoQuadro, codigoCartao, novaColuna);
-        cout << "Cart„o movido com sucesso para a coluna " << novaColuna << endl;
+        cout << "Cart√£o movido com sucesso para a coluna " << novaColuna << endl;
 
     } catch (const exception& exp) {
-        cout << "Erro ao mover cart„o: " << exp.what() << endl;
+        cout << "Erro ao mover cart√£o: " << exp.what() << endl;
     }
 }
 
 
 void CntrIUQuadro::eliminarCartao(const string& codigoQuadro, const string& codigoCartao) {
     try {
-        // 1. Verificar se os cÛdigos s„o v·lidos.
+        // 1. Verificar se os c√≥digos s√£o v√°lidos.
         if (!Codigo::validarCodigo(codigoQuadro) || !Codigo::validarCodigo(codigoCartao)) {
-            throw invalid_argument("CÛdigos inv·lidos para eliminar o cart„o.");
+            throw invalid_argument("C√≥digos inv√°lidos para eliminar o cart√£o.");
         }
 
-        // 2. Chamar o mÈtodo da camada de lÛgica de negÛcios para eliminar o cart„o.
+        // 2. Chamar o m√©todo da camada de l√≥gica de neg√≥cios para eliminar o cart√£o.
         cntrLNQuadro->eliminarCartao(codigoQuadro, codigoCartao);
-        cout << "Cart„o eliminado com sucesso!" << endl;
+        cout << "Cart√£o eliminado com sucesso!" << endl;
 
     } catch (const exception& exp) {
-        cout << "Erro ao eliminar cart„o: " << exp.what() << endl;
+        cout << "Erro ao eliminar cart√£o: " << exp.what() << endl;
     }
+    // CntrLNQuadro.cpp
+
+
+
+class CntrLNQuadro : public ILNQuadro {
+public:
+    void visualizarQuadro(const std::string& codigo) override {
+        // Implementa√ß√£o do m√©todo visualizarQuadro
+    }
+
+    // Implementa√ß√µes dos outros m√©todos da interface
+};
+class CntrIUQuadro : public IUQuadro {
+private:
+    ILNQuadro* cntrLNQuadro;
+
+public:
+    // ... (outros m√©todos)
+
+    void visualizarQuadro(const string& codigo) override {
+        cntrLNQuadro->visualizarQuadro(codigo);
+    }
+};
 }
 
