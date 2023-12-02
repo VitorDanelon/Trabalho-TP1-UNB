0bba23f0ac9d1005328cc5857ec12f370c62e2f8
Luca Verdade Lenzoni <lucaverdade@gmail.com>
2023-12-02 19:37:54 -0300

    helpppp

--- IAG.h
+++ IAG.h
@@ -20,7 +20,6 @@ class ISGestor;
 
 class IAG {
 public:
-    virtual bool autenticar(Email& email, Senha& senha) = 0;
     virtual void exibirQuadrosECartoes(const Email& email) = 0;
     virtual void criarQuadro(const std::string& codigo, const std::string& nome, const std::string& descricao, const std::string& limite) = 0;
     virtual void visualizarQuadro(const std::string& codigo) = 0;
@@ -34,7 +33,6 @@ public:
 
 class ISGestor {
 public:
-    virtual bool autenticar(Email* email) = 0;
     virtual void exibirQuadrosECartoes(const Email& email) = 0;
     virtual void criarQuadro(const std::string& codigo, const std::string& nome, const std::string& descricao, const std::string& limite) = 0;
     virtual void visualizarQuadro(const std::string& codigo) = 0;
@@ -47,67 +45,3 @@ public:
 };
 
 #endif // IAG_H_INCLUDEDD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// -------------------------------------------------------------------------------------------
-
-class ISQuadro;
-class ISCartao;
-
-// -------------------------------------------------------------------------------------------
-// Declaração de interface para serviço de quadro provido pela camada de apresentação.
-
-class IAGestor {
-public:
-    virtual void criarQuadro(const string& codigo, const string& nome, const string& descricao, const string& limite) = 0;
-    virtual void visualizarQuadro(const string& codigo) = 0;
-    virtual void eliminarQuadro(const string& codigo) = 0;
-    virtual void setCntrISQuadro(ISQuadro *cntrISQuadro) = 0;
-    virtual void criarCartao(const string& codigoQuadro, const string& codigoCartao, const string& nome, const string& descricao, const string& coluna) = 0;
-    virtual void visualizarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual void moverCartao(const string& codigoQuadro, const string& codigoCartao, const string& novaColuna) = 0;
-    virtual void eliminarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual void setCntrLNCartao(ISCartao *cntrISCartao) = 0;
-    virtual ~IAGestor() {}  // Método destrutor virtual.
-};
-
-// -------------------------------------------------------------------------------------------
-// Declaração de interface para serviço de quadro provido pela camada de serviço.
-
-class ISQuadro {
-public:
-    virtual void criarQuadro(const string& codigo, const string& nome, const string& descricao, const string& limite) = 0;
-    virtual void visualizarQuadro(const string& codigo) = 0;
-    virtual void eliminarQuadro(const string& codigo) = 0;
-    virtual ~ISQuadro() {}  // Método destrutor virtual.
-};
-
-// -------------------------------------------------------------------------------------------
-// Declaração de interface para serviço de cartao provido pela camada de serviço.
-
-class ISCartao {
-public:
-    virtual void criarCartao(const string& codigoQuadro, const string& codigoCartao, const string& nome, const string& descricao, const string& coluna) = 0;
-    virtual void visualizarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual void moverCartao(const string& codigoQuadro, const string& codigoCartao, const string& novaColuna) = 0;
-    virtual void eliminarCartao(const string& codigoQuadro, const string& codigoCartao) = 0;
-    virtual ~ISCartao(){}  // Método destrutor virtual.
-};
-
-#endif */
--- main.cpp
+++ main.cpp
@@ -55,96 +55,29 @@ int main() {
 
    return 0;
 
+   // Criar instância do stub
+    StubISAutenticacao stubAutenticacao;
+    StubIAG stubGestor;
 
-}
-/*
+    // Configurar o gestor de autenticação
+    stubGestor.setCntrISAutenticacao(&stubAutenticacao);
 
+    // Realizar a autenticação
+    Email emailAutenticado("email@dominio.com");
+    Senha senhaAutenticacao("senha123");
+    bool autenticado = stubAutenticacao.autenticar(emailAutenticado, senhaAutenticacao);
 
-
-int main() {
-    StubGestor stub;
-
-    // Exemplo de utilização do stub
-    stub.criarQuadro("Q001", "Quadro 1", "Descrição Quadro 1", "10");
-    stub.visualizarQuadro("Q001");
-    stub.eliminarQuadro("Q001");
-
-    stub.criarCartao("Q001", "C001", "Cartão 1", "Descrição Cartão 1", "TO_DO");
-    stub.visualizarCartao("Q001", "C001");
-    stub.moverCartao("Q001", "C001", "EM_EXECUCAO");
-    stub.eliminarCartao("Q001", "C001");
-
-    return 0;
-}
-
-
-
-
-
-
-   /*
-int main() {
-
-
-
-    // Aqui você precisará criar instâncias necessárias, incluindo o controlador.
-    IAGestor* cntrIAGestor = new CntrIAGestor();  // Substitua com a implementação real.
-    IApresentacao* apresentacao = new Apresentacao(cntrIAGestor);
-
-    // Chame o método para apresentar quadros e cartões com um email genérico.
-    apresentacao->apresentarQuadrosECartoes("testw@exemplo.com");
-
-
-    //delete apresentacao;
-   // delete cntrIAGestor;
-
-    return 0;
-
-
-
-
-    // ---------------------------------------------------------------------------------
-    // Instanciar classe controladora e classe stub.
-
-    IUQuadro* cntrIAQuadro = new CntrIAQuadro();
-    ISQuadro* stubISQuadro = new StubISQuadro();
-
-    // Ligar instância de controladora a instância de stub.
-
-    cntrIAQuadro->setCntrISQuadro(stubISQuadro);
-
-    // Simular a apresentação de tela inicial do sistema.
-    // Vamos assumir que a autenticação já foi realizada.
-
-    string codigoQuadro;
-    string codigoCartao;
-    string nome;
-    string descricao;
-    string limite;
-
-    // Exemplo de criação de quadro.
-    try {
-        cout << "Criar Quadro:" << endl;
-        cout << "Digite o código do quadro: ";
-        cin >> codigoQuadro;
-        cout << "Digite o nome do quadro: ";
-        cin >> nome;
-        cout << "Digite a descrição do quadro: ";
-        cin >> descricao;
-        cout << "Digite o limite do quadro: ";
-        cin >> limite;
-
-        cntrIAGestor->criarQuadro(codigoQuadro, nome, descricao, limite);
-    } catch (const invalid_argument& exp) {
-        cout << "Erro ao criar quadro: " << exp.what() << endl;
+    if (autenticado) {
+        // Se autenticado, exibir quadros e cartões genéricos
+        stubGestor.exibirQuadrosECartoes(emailAutenticado);
+    } else {
+        cout << "Autenticação falhou." << endl;
     }
 
-    // Outras operações podem ser adicionadas conforme necessário.
-
-    // Destruir instância de controladora e instância de stub.
-
-    delete cntrIAGestor;
-    delete stubISQuadro;
-
     return 0;
-} */
+
+
+
+
+
+}
